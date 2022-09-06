e275def0b05ff47d3168ff085efe4624b27a0518
vlados552 <vlados552@gmail.com>
2022-09-06 16:07:39 -0700

    Add functionality for action buttons

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 /*----- constants -----*/
 const DIFFICULT = 20;
+const SPEED_AGE = 5 * 1000;
+const SPEED_HUNGER = 2 * 1000;
+const SPEED_BOREDOM = 1 * 1000;
+const SPEED_SLEEP = 4 * 1000;
 /*----- app's state (variables) -----*/
 let gameState;
+const pet = new Object();
 /*----- cached element references -----*/
+body = document.querySelector('body');
 sectionStart = document.querySelector('.js-section-start');
 sectionGame = document.querySelector('.js-section-game');
 sectionEnd = document.querySelector('.js-section-end');
 buttonStart = document.querySelector('#buttonStart');
 buttonPlayAgain = document.querySelector('#buttonPlayAgain');
-body = document.querySelector('body');
+statAge = document.querySelector('#statAge');
+statHunger = document.querySelector('#statHunger');
+statBoredom = document.querySelector('#statBoredom');
+statSleep = document.querySelector('#statSleep');
+actionFeed = document.querySelector('#actionFeed');
+actionPlay = document.querySelector('#actionPlay');
+actionSleep = document.querySelector('#actionSleep');
 /*----- event listeners -----*/
 body.addEventListener('click', function (e) {
 	if (e.target === buttonStart) {
@@ -19,13 +31,27 @@ body.addEventListener('click', function (e) {
 		gameState = 0;
 		render();
 	}
+	if (e.target === actionFeed) {
+		decreaseHunger(pet);
+	}
+	if (e.target === actionPlay) {
+		decreaseBoredom(pet);
+	}
+	if (e.target === actionSleep) {
+		decreaseSleep(pet);
+	}
 });
 /*----- functions -----*/
 function init() {
+	pet.age = 0;
+	pet.hunger = 0;
+	pet.boredom = 0;
+	pet.sleep = 0;
 	render();
 }
 
 function render() {
+	checkDifficult(pet);
 	if (gameState === 0) {
 		setHidden(sectionGame);
 		setHidden(sectionEnd);
@@ -39,6 +65,10 @@ function render() {
 		setHidden(sectionGame);
 		removeHidden(sectionEnd);
 	}
+	statAge.innerText = `Age: ${pet.age}`;
+	statHunger.innerText = `Age: ${pet.hunger}`;
+	statBoredom.innerText = `Age: ${pet.boredom}`;
+	statSleep.innerText = `Age: ${pet.sleep}`;
 }
 
 function toggleHidden(target) {
@@ -49,4 +79,43 @@ function setHidden(target) {
 }
 function removeHidden(target) {
 	target.classList.remove('hidden');
+}
+function increaseHunger(obj) {
+	obj.hunger += 1;
+	render();
+}
+function decreaseHunger(obj) {
+	obj.hunger -= 1;
+	render();
+}
+function increaseBoredom(obj) {
+	obj.boredom += 1;
+	render();
+}
+function decreaseBoredom(obj) {
+	obj.boredom -= 1;
+	render();
+}
+function increaseSleep(obj) {
+	obj.sleep += 1;
+	render();
+}
+function decreaseSleep(obj) {
+	obj.sleep -= 1;
+	render();
+}
+function increaseAge(obj) {
+	obj.age += 1;
+	render();
+}
+function checkDifficult(obj) {
+	if (obj.hunger >= DIFFICULT) {
+		gameState = 2;
+	}
+	if (obj.boredom >= DIFFICULT) {
+		gameState = 2;
+	}
+	if (obj.sleep >= DIFFICULT) {
+		gameState = 2;
+	}
 }
